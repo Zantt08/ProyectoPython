f564f67622a52081087009c0ebe21e5ed9637d3b
Santiago Zalazar <zalazarmail08@gmail.com>
2025-07-04 17:28:07 -0300

    about me page completed

--- AppWeb/views.py
+++ AppWeb/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -23,3 +24,21 @@ def login_usuario(request):
             messages.error(request, 'Usuario o contraseña incorrectos')
 
     return render(request, 'AppWeb/login.html')
+
+def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmar = request.POST['confirmar']
+
+        if password != confirmar:
+            messages.error(request, "Las contraseñas no coinciden.")
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, "Ese nombre de usuario ya está en uso.")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('inicio')
+    return render(request, 'AppWeb/sign_up.html')
--- ProyectoWeb1/urls.py
+++ ProyectoWeb1/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from AppWeb import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),
     path('login/', views.login_usuario, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='inicio'), name='logout'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
